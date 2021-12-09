2a53d2fa43ceaea6d9d87ba5452b69bada079e7c
shil-alone <shilalone007@gmail.com>
2021-12-09 12:28:09 +0530

    implemented pdf viewer

--- app/src/main/java/com/codershil/scanx/ConvertToPdfActivity.java
+++ app/src/main/java/com/codershil/scanx/ConvertToPdfActivity.java
@@ -2,6 +2,7 @@ package com.codershil.scanx;
 
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
 
@@ -9,6 +10,7 @@ import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.ContentResolver;
 import android.content.ContentValues;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.pdf.PdfDocument;
 import android.net.Uri;
@@ -37,7 +39,7 @@ public class ConvertToPdfActivity extends AppCompatActivity implements ImageAdap
     private ImageAdapter imageAdapter;
     private ArrayList<Uri> selectedImageList = new ArrayList<>();
     private ActivityResultLauncher<String> galleryLauncher;
-    Uri imageUri;
+    Uri imageUri, pdfUri;
     String fileName;
 
     @Override
@@ -47,9 +49,9 @@ public class ConvertToPdfActivity extends AppCompatActivity implements ImageAdap
         setContentView(binding.getRoot());
         binding.progressBar.setVisibility(View.GONE);
 
-        // setting up recycler view
         Bundle bundle = getIntent().getExtras();
         selectedImageList = (ArrayList<Uri>) bundle.get("imageUriData");
+
         GridLayoutManager gridLayoutManager = new GridLayoutManager(ConvertToPdfActivity.this, 3);
         imageAdapter = new ImageAdapter(selectedImageList, ConvertToPdfActivity.this, this);
         binding.selectedImagesRV.setLayoutManager(gridLayoutManager);
@@ -85,11 +87,11 @@ public class ConvertToPdfActivity extends AppCompatActivity implements ImageAdap
                     @Override
                     public void onClick(View v) {
                         String nameOfFile = edtFileName.getText().toString();
-                        if (nameOfFile.isEmpty()){
+                        if (nameOfFile.isEmpty()) {
                             edtFileName.setError("please enter name first");
                             return;
                         }
-                        fileName =  nameOfFile + ".pdf";
+                        fileName = nameOfFile + ".pdf";
                         startPdfConversion(dialog);
                     }
                 });
@@ -121,6 +123,17 @@ public class ConvertToPdfActivity extends AppCompatActivity implements ImageAdap
 
     }
 
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        outState.putParcelableArrayList("uriList", selectedImageList);
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        selectedImageList = savedInstanceState.getParcelableArrayList("uriList");
+    }
 
     // a method that converts image into pdf format and saves it into external storage
     public void convertImageToPdf(ArrayList<Uri> uriList) {
@@ -156,7 +169,7 @@ public class ConvertToPdfActivity extends AppCompatActivity implements ImageAdap
             contentValues.put(MediaStore.MediaColumns.MIME_TYPE, "files/pdf");
             contentValues.put(MediaStore.MediaColumns.RELATIVE_PATH, Environment.DIRECTORY_DOCUMENTS + File.separator + "ScanX" + File.separator + "Pdf Documents");
 
-            Uri pdfUri = contentResolver.insert(MediaStore.Files.getContentUri("external"), contentValues);
+            pdfUri = contentResolver.insert(MediaStore.Files.getContentUri("external"), contentValues);
             try {
                 outputStream = contentResolver.openOutputStream(pdfUri);
                 pdfDocument.writeTo(outputStream);
@@ -183,6 +196,9 @@ public class ConvertToPdfActivity extends AppCompatActivity implements ImageAdap
                 Toast.makeText(ConvertToPdfActivity.this, "pdf saved to \"Documents/ScanX/Pdf Documents\"", Toast.LENGTH_LONG).show();
                 binding.progressBar.setVisibility(View.GONE);
                 binding.btnConvertToPdf.setEnabled(true);
+                Intent intent = new Intent(ConvertToPdfActivity.this, PdfViewerActivity.class);
+                intent.putExtra("pdfUri", pdfUri);
+                startActivity(intent);
             }
         });
     }
---
+++ app/src/main/java/com/codershil/scanx/PdfViewerActivity.java
@@ -0,0 +1,59 @@
+package com.codershil.scanx;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.database.Cursor;
+import android.graphics.Canvas;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.OpenableColumns;
+import android.widget.Toast;
+
+import com.github.barteksc.pdfviewer.PDFView;
+import com.github.barteksc.pdfviewer.listener.OnDrawListener;
+import com.github.barteksc.pdfviewer.listener.OnErrorListener;
+import com.github.barteksc.pdfviewer.listener.OnLoadCompleteListener;
+import com.github.barteksc.pdfviewer.listener.OnPageChangeListener;
+import com.github.barteksc.pdfviewer.listener.OnPageErrorListener;
+import com.github.barteksc.pdfviewer.listener.OnPageScrollListener;
+import com.github.barteksc.pdfviewer.listener.OnRenderListener;
+import com.github.barteksc.pdfviewer.scroll.DefaultScrollHandle;
+
+import java.util.ArrayList;
+
+public class PdfViewerActivity extends AppCompatActivity {
+    PDFView pdfView;
+    Uri pdfUri;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pdf_viewer);
+        pdfView = findViewById(R.id.pdfView);
+
+        Bundle bundle = getIntent().getExtras();
+        pdfUri = (Uri) bundle.get("pdfUri");
+        displayFromUri(pdfUri);
+    }
+
+    private void displayFromUri(Uri uri) {
+        pdfView.fromUri(uri)
+                .defaultPage(0)
+                .enableAnnotationRendering(true)
+                .scrollHandle(new DefaultScrollHandle(this))
+                .spacing(10) // in dp
+                .onError(new OnErrorListener() {
+                    @Override
+                    public void onError(Throwable t) {
+                        Toast.makeText(PdfViewerActivity.this, "error", Toast.LENGTH_LONG).show();
+                    }
+                })
+                .onPageError(new OnPageErrorListener() {
+                    @Override
+                    public void onPageError(int page, Throwable t) {
+                        Toast.makeText(PdfViewerActivity.this, "page error", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .load();
+    }
+    
+}
